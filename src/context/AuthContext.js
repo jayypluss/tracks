860ae0971019c2e772346c06aca9217623435e60
Jonathan Ferreira Kuntz <jonathanfk1@gmail.com>
2021-05-14 13:51:35 -0300

    Recover progress files.

---
+++ src/context/AuthContext.js
@@ -0,0 +1,50 @@
+import AsyncStorage from "@react-native-community/async-storage";
+import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
+import { navigate } from "../navigationRef";
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
+    case "signup":
+      return { errorMessage: "", token: action.payload };
+    default:
+      return state;
+  }
+};
+
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post("/signup", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signup", payload: response.data.token });
+
+    navigate("TrackList");
+  } catch (err) {
+    dispatch({
+      type: "add_error",
+      payload: "Something went wrong with sign up",
+    });
+  }
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message (somehow)
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // somehow sign out!!!
+  };
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signin, signout, signup },
+  { token: null, errorMessage: "" }
+);
