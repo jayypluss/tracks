826da07da64418f6d657b21378bcdc7538cf2b51
Jonathan Ferreira Kuntz <jonathanfk1@gmail.com>
2021-06-02 07:46:47 -0300

    Up to 258.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -9,14 +9,14 @@ import useLocation from '../hooks/useLocation';
 import TrackForm from '../components/TrackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { state, addLocation } = useContext(LocationContext);
+  const { state: { recording }, addLocation } = useContext(LocationContext);
 
   const callback = useCallback(location => {
-    addLocation(location, state.recording);
-  }, [state.recording]);
+    addLocation(location, recording);
+  }, [recording]);
 
 
-  const [err] = useLocation(isFocused || state.recording, callback);
+  const [err] = useLocation(isFocused || recording, callback);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
